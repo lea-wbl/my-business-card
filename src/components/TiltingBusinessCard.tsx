7c581a800b6b6a37acf528f85b20b0f8a184e2d0
Léa Weibel <lea@Leas-Mac-mini.local>
2025-05-08 17:38:18 +0200

    added forgotten favicon

--- src/components/TiltingBusinessCard.tsx
+++ src/components/TiltingBusinessCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { FiGithub, FiGlobe, FiLinkedin } from "react-icons/fi";
 
 export default function TiltingBusinessCard() {
